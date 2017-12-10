33d94a2ce39581483aa3bc932ee05f37f87f1bec
Robert Watson <spartan83@gmail.com>
2017-12-10 19:45:03 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,6 +10,14 @@ def sort_array_char_count (array)
 array.sort_by {|word| word.length}
 end
 
-def swap_elements
+def swap_elements (array, index, destination_index)
+a= array[index]
+b= array[destination_index]
+array[index]=b
+array[destination_index]=a
+array
+
+
+end
 
 end
